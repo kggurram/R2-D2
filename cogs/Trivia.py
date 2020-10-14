1a18002524fcb08cd8a3fd6fd4c6fd9561ded1b5
DaoudAli <daoudson0@gmail.com>
2020-10-13 23:30:50 -0400

    Updated Trivia Cog

--- cogs/Trivia.py
+++ cogs/Trivia.py
@@ -6,11 +6,10 @@ class Trivia(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command
-async def trivia(self, ctx):
-    await ctx.send("Welcome to Star Wars Trivia! Answer by entering the letter answer below! Let's Begin.")
+    @commands.command()
+    async def trivia(self, ctx):
+        await ctx.send("Welcome to Star Wars Trivia! Answer by entering the letter answer below! Let's Begin.")
 
-    pass
 
 #required setup def
 def setup(bot):
