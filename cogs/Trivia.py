836cf39a03d68a874b72267f60084cbf975f918a
DaoudAli <daoudson0@gmail.com>
2020-10-16 17:38:57 -0400

    First Working Version of Trivia

--- cogs/Trivia.py
+++ cogs/Trivia.py
@@ -7,8 +7,12 @@ class Trivia(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
+
     @commands.command()
     async def trivia(self, ctx):
+        global answer   
+
         await ctx.send("Welcome to Star Wars Trivia! Answer by entering the letter answer below! Let's Begin.")
 
         #Trivia Questions in the form of Question, 4 Options, Answer
@@ -62,18 +66,31 @@ class Trivia(commands.Cog):
       
         #Choose a random question number from the list of questions
         q_num = (random.choice(question_list))                             
-        question, opt1, opt2, opt3, opt4, answer = q_num[0], q_num[1],q_num[2],q_num[3],q_num[4], q_num[5]    
+        question, opt1, opt2, opt3, opt4, answer = q_num[0], q_num[1],q_num[2],q_num[3],q_num[4], q_num[5]  
+        
 
         #Ask Question/Display Options to channel
-        await ctx.send(f'{question}\n{opt1}\n{opt2}\n{opt3}\n{opt4}')      
+        await ctx.send(f'{question}\n{opt1}\n{opt2}\n{opt3}\n{opt4}\n\nType in your answer below starting with ! (for example "!A" for option A) ') 
+          
 
-        #Recieve user input and check with answer
-         
-         
-       
-    
-  
+    #Recieve user input and check with answer
+    @commands.Cog.listener()
+    async def on_message(self, message):
+      channel = message.channel
+      if (message.author == self.bot.user):
+        return
+      if (message.content[0] == "!" and len(message.content) == 2):
+        if (message.content[1].upper() == answer):
+          await channel.send("Correct! I praise your galatic knowledge. Thank you for playing!")
+          return
+        await channel.send("Incorrect. Please prepare for the battle next time.")
+
+        
+        
+
 
 #required setup def
 def setup(bot):
     bot.add_cog(Trivia(bot))
+
+answer = ""
