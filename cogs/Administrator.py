82ee62ea8a9d107d526d34a8d6d58579c5f03479
Karthik <kggurram@gmail.com>
2021-01-14 23:23:35 -0500

    Role update
    Added a couple of roles, NOTE: kicking no longer works, fix for future commit

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -11,20 +11,20 @@ class Administrator(commands.Cog):
     
     #kick command (only for Admin, Mod, or Bot roles)
     @commands.command()
-    @commands.has_any_role("Administrator, Moderator, Bot") #if user has given role
+    @commands.has_any_role("Supreme Leaders, Emperor, Administrator, Admin, Moderator, Bot") #if user has given role
     async def kick(self, ctx, member : discord.Member, *, reason=None):
         await member.kick(reason=reason) #kick
     
     #ban command (only for Admin, Mod, or Bot roles)
     @commands.command()
-    @commands.has_any_role("Administrator, Moderator, Bot") #if user has given role
+    @commands.has_any_role("Supreme Leaders, Emperor, Administrator, Admin, Moderator, Bot") #if user has given role
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         await member.ban(reason=reason) #ban
         await ctx.send(f"{member.mention}, twee-vwoop ZZT! >:(") #R2-D2 sends an angry message
 
     #unban command (only for Admin, Mod, or Bot roles)
     @commands.command()
-    @commands.has_any_role("Administrator, Moderator, Bot") #if user has given role
+    @commands.has_any_role("Supreme Leaders, Emperor, Administrator, Admin Moderator, Bot") #if user has given role
     async def unban(self, ctx, *, member):
         banned_users = await ctx.guild.bans() #list of banned members
         member_name, member_discriminator = member.split("#") #splits characters around hashtag ie. XXXXX#12345 --> name = XXXXXX, discriminator = 12345
